3d1d47cf83b9c1e2c12d978997eb129e55972224
Dimitri-Bit <dimitrijefustic0@gmail.com>
2024-05-28 02:36:46 +0200

    started engine rebuild

--- engine.cpp
+++ engine.cpp
@@ -1,110 +1,55 @@
-/*
-    A lot of the chess.hpp library usage and engine knowladge has been taken from Raphael
-    GitHub: https://github.com/Orbital-Web/Raphael/tree/main
-*/
+#include "libraries/chess.hpp"
+#include <map>
+#include <string>
 
-#include "search.hpp"
-#include "constants.hpp"
-#include <iostream>
-#include <chrono>
+using namespace chess;
 
-#define WHITE_COLOR chess::Color::WHITE
-#define BLACK_COLOR chess::Color::BLACK
+void printBoard(Board &board, Color color);
+Square getSquareByColor(Color color, int row, int col);
 
-chess::Color computerColor;
-
-void getUserInput();
-chess::Move getBestMove(chess::Board &board);
-uint64_t getCurrentTime();
+std::map<chess::Piece, std::string> piece_map =
+{
+    {Piece::BLACKPAWN, "p"},
+    {Piece::BLACKKNIGHT, "k"},
+    {Piece::BLACKROOK, "r"},
+    {Piece::BLACKBISHOP, "b"},
+    {Piece::BLACKQUEEN, "q"},
+    {Piece::BLACKKING, "k"},
+    {Piece::WHITEPAWN, "P"},
+    {Piece::WHITEKNIGHT, "K"},
+    {Piece::WHITEROOK, "R"},
+    {Piece::WHITEBISHOP, "B"},
+    {Piece::WHITEQUEEN, "Q"},
+    {Piece::WHITEKING, "K"},
+    {Piece::NONE, " "}
+};
 
 int main() {
-    chess::Board board = chess::Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    Board board = Board(constants::STARTPOS);
 
-    getUserInput();
+    printBoard(board, Color::WHITE);
 
-    /* Clear std::cin */
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    while (true) {
-        if (board.sideToMove() == computerColor) {
-            chess::Move move = getBestMove(board);
-            board.makeMove(move);
-        } else {
-            std::string moveString;
-            std::cout << "What is your move?" << std::endl;
-            std::getline(std::cin, moveString);
-            board.makeMove(chess::uci::parseSan(board, moveString));
-        }
-
-        chess::utils::printBitboard(board.all());
-        std::cout << "FEN: " << board.getFen() << std::endl;
-    }
     return 0;
 }
 
-chess::Move getBestMove(chess::Board &board) {
-    uint64_t start = getCurrentTime();
-    chess::Movelist moves;
-    chess::movegen::legalmoves(moves, board);
-
-    int bestScore;
-    chess::Move bestMove;
-
-    if(computerColor == WHITE_COLOR) {
-        bestScore = INT_MIN;
-
-        for (int i = 0; i < moves.size(); i++) {
-            const auto move = moves[i];
-            board.makeMove(move);
-            int score = minimax(board, MAX_DEPTH, INT_MIN, INT_MAX, true);
-            board.unmakeMove(move);
-
-            if (score > bestScore) {
-                bestScore = score;
-                bestMove = move;
-                std::cout << "Best Score (" << bestScore << ")" << std::endl;
-            }
+/*
+    Depending on who's perspective the printed board should be based off of
+    we provide a color argument which is the color of the human playing the bot
+*/
+void printBoard(Board& board, Color color) {
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            Piece piece = board.at(getSquareByColor(color, row, col));
+            std::cout << " " << piece_map[piece]; 
         }
-    } else {
-        bestScore = INT_MAX;
-
-        for (int i = 0; i < moves.size(); i++) {
-            const auto move = moves[i];
-            board.makeMove(move);
-            int score = minimax(board, MAX_DEPTH, INT_MIN, INT_MAX, false);
-            board.unmakeMove(move);
-
-            if (score < bestScore) {
-                bestScore = score;
-                bestMove = move;
-                std::cout << "Best Score (" << bestScore << ")" << std::endl;
-            }
-        }
-    }
-
-    int result = getCurrentTime() - start;
-    std::cout << std::endl;
-    std::cout << "Best Move (" << chess::uci::moveToLan(board, bestMove) << ")" << std::endl;
-    std::cout << "Best Score (" << bestScore << ")" << std::endl;
-    std::cout << "Time (" << result << "ms)" << std::endl;
-    std::cout << "Depth (" << MAX_DEPTH << ")\n" << std::endl;
-    return bestMove;
-}
-
-void getUserInput() {
-    char side;
-    std::cout << "Which side is the computer playing? W/B" << std::endl;
-    std::cin >> side;
-    
-    if((char) tolower(side) == 'w') {
-        computerColor = WHITE_COLOR;
-        std::cout << "Computer will be playing White" << std::endl;
-    } else {
-        computerColor = BLACK_COLOR;
-        std::cout << "Computer will be playing Black" << std::endl;
+        std::cout << std::endl;
     }
 }
 
-uint64_t getCurrentTime() {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+Square getSquareByColor(Color color, int row, int col) {
+    if (color == Color::WHITE) {
+        return Square(63- (row * 8 + col));
+    } else {
+        return Square(row * 8 + col);
+    }
 }
